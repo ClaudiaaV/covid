1278cbf67479ff46165b85d9cc915c8d16924680
Claudia Mihaela Vasile <claudia.mihaela.vasile@ibm.com>
2021-05-28 16:08:26 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,150 @@
+function getTable() {
+
+ const columnDefs = [
+    { field: "county_code",maxWidth: 110 },
+    { field: "county",maxWidth: 120  },
+    { field: "total_county",maxWidth: 180 },
+    { field: "total_healed",maxWidth: 100 },
+    { field: "total_dead",maxWidth: 100 }
+    
+  ];
+
+
+  const gridOptions = {
+    columnDefs: columnDefs,
+    rowSelection: 'multiple'
+  };
+
+// lookup the container we want the Grid to use
+const eGridDiv = document.querySelector('#myGrid');
+
+// create the grid passing in the div to use together with the columns & data we want to use
+new agGrid.Grid(eGridDiv, gridOptions);
+
+// fetch the row data to use and one ready provide it to the Grid via the Grid API
+agGrid.simpleHttpRequest({url: 'https://covid19.geo-spatial.org/api/dashboard/getCasesByCounty'})
+    .then(data => {
+        console.log(data)
+        gridOptions.api.setRowData(data.data.data);
+    });
+    var makeCol = gridOptions.columnApi.getColumn("county_code")
+    makeCol.colDef.headerName="Cod Judet";
+    var makeCol = gridOptions.columnApi.getColumn("county")
+makeCol.colDef.headerName="Judet";
+
+var makeCol = gridOptions.columnApi.getColumn("total_county")
+makeCol.colDef.headerName="Numar total de cazuri";
+
+var makeCol = gridOptions.columnApi.getColumn("total_healed")
+makeCol.colDef.headerName="Vindecati";
+
+var makeCol = gridOptions.columnApi.getColumn("total_dead")
+makeCol.colDef.headerName="Decedati";
+gridOptions.api.refreshHeader();
+
+}
+
+
+function getCases() {
+    
+    const api = `https://covid19.geo-spatial.org/api/dashboard/getDailyCases`;
+    
+
+    fetch(api)
+        .then(response => {
+            return response.json();
+        })
+        .then(data => {
+        console.log(data);
+       
+        document.getElementById("demo7").innerHTML ="Data: " + data.data.data[454].Data;
+        document.getElementById("demo").innerHTML ="Cazuri înregistrate: " + data.data.data[454].Cazuri;
+        document.getElementById("demo8").innerHTML ="Cazuri active:  " + data.data.data[454]["Cazuri active"];
+        
+        document.getElementById("demo6").innerHTML ="Număr total cazuri: " + data.data.data[454].Total;
+        document.getElementById("demo4").innerHTML ="Pacienți la terapie intensivă: " + data.data.data[454]["Terapie intensiva"];
+        document.getElementById("demo9").innerHTML ="Număr persoane decedate: " + data.data.data[454].Morti;
+        document.getElementById("demo10").innerHTML ="Număr teste efectuate: " + data.data.data[454]["Nr de teste pe zi"];
+        document.getElementById("demo11").innerHTML ="Procent teste pozitive: " + data.data.data[454]["Procentaj de teste pozitive"] + "%";
+        document.getElementById("demo12").innerHTML ="Persoane vindecate: " + data.data.data[454]["Vindecati pe zi"];
+        document.getElementById("demo13").innerHTML ="Total persoane vindecate: " + data.data.data[454].Vindecati;
+
+    })
+          
+    }      
+
+getCases()
+
+
+
+
+
+
+
+/* var kidsLabel = [], femininLabel = [], menLabel=[], dataLabel =[]
+
+async function  dataChart (){
+await getData()
+    let ctx = document.getElementById('myChart').getContext('2d');
+    let myChart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+        labels: dataLabel,
+        datasets: [{
+            label: 'Numar de copii afectați',
+            data: kidsLabel,
+            backgroundColor:'blue',
+            borderColor:'rgba(255, 99, 132, 1)',  
+        },
+        {
+            label: 'Numar de bărbați afectați',
+            data: menLabel,
+            backgroundColor:'pink',
+            borderColor:'rgba(255, 99, 132, 1)',  
+        },
+        {
+            label: 'Numar de femei afectate',
+            data: femininLabel,
+            backgroundColor:'pink',
+            borderColor:'rgba(255, 99, 132, 1)',  
+        }
+    ]
+
+    },
+
+
+    options: {
+       tooltips: {
+           mode: 'index'
+       }
+    }
+});
+}
+
+dataChart()
+ */
+
+  /* //fetch Data from API
+
+  async function getData(){
+const apiUrl = `https://covid19.geo-spatial.org/api/dashboard/getPercentageByGender`;
+
+
+const response = await fetch(apiUrl)
+const barChartData = await response.json()
+console.log(barChartData)
+
+
+const kids = barChartData.data.copii 
+const feminin = barChartData.data.feminin
+const men = barChartData.data.masculin
+const data = barChartData.data.timestamp
+console.log(men,kids, feminin)
+
+kidsLabel = kids
+femininLabel = feminin
+masculinLabel = men
+dataLabel = data
+  } */
+    
+    
